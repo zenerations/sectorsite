8391a318434af964d8893d693fbc7e697b2028e8
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-11-11 18:36:27 -0500

    push landing design to prod

--- components/primary-card.tsx
+++ components/primary-card.tsx
@@ -6,20 +6,8 @@ import Image from "next/image";
 const PrimaryCard = ()=>{
   return (
     <div className=" bg-brand-2 rounded-3xl mx-4 max-w-xl">
-      <div className="flex flex-col flex-nowrap m-4 ">
-        <Image src={"/images/sectorproject.png"} width={108} height={135} alt="The Sector Project Logo" />
-        <b>
-          <p className="mx-16">
-            How does Generation Z envision our world in twenty years? As an
-            emerging age group that is not only purpose-driven and empathetic,
-            but maintains the hope to see young people grow up towards a safer,
-            more inclusive, and equitable future, we prioritize ACTION, WORK,
-            and IMPACT. From here on out, we seek to make that shift.
-            Zenerations is launching a new 4-part initiative to add a fully
-            action-based component to our organization in tandem with content
-            creation.
-          </p>
-        </b>
+      <div className="">
+        <Image src={"/images/sectorproject.png"} width={400} height={500} alt="The Sector Project Logo" />
       </div>
     </div>
   );
