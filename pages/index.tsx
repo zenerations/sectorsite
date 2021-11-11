3c30abb1cf137c5b5c33d1f3fa551342ed9ed32b
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-11-11 00:54:54 -0500

    basic homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,18 @@ import Image from 'next/image'
 
 import BG from 'layouts/bg'
 import Meta from 'components/meta'
+import PrimaryCard from 'components/primary-card'
 
 const Home = () => {
   return (
-  <div>
-    <Meta />
-      <main className="home-bg pagefill">
-        <h1>ZENERATIONS</h1>
+    <div>
+      <Meta />
+      <main className="home-bg bg-fixed bg-center bg-cover pagefill overflow-hidden grid place-content-center text-brand-8 ">
+        <div>
+          <PrimaryCard />
+        </div>
       </main>
-  </div>
+    </div>
   );
 }
 
