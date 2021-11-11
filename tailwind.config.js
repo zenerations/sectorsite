2b3a04261ff3a9a0952ac29c95ae10a33db1630a
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-11-10 21:34:19 -0500

    style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,28 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./layouts/**/*.{js,ts,jsx,tsx}",
   ],
-  mode: "jit",
   darkMode: false, // or 'media' or 'class'
+  mode: 'jit',
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        brand:{
+          1:"#d1e1ef",
+          3:"#bdd4e6",
+          2:"#a6c5dd",
+          4:"#81acce",
+          5:"#6f9abc",
+          6:"#4f7898",
+          7:"#355269",
+          8:"#283c4c",
+        }
+      }
+    },
   },
   variants: {
     extend: {},
